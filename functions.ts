553f64590da722197e9540456eabede0e554a0e6
Jan <jjirsik@gmail.com>
2020-01-25 12:39:31 +0100

    type unknown

--- functions.ts
+++ functions.ts
@@ -1,11 +1,12 @@
 type Combinable = number | string; // my own type
 type ResultFormatType = "as-num" | "as-str";
 
-const combine = (
+let combine: (a: Combinable, b: Combinable, r: ResultFormatType) => Combinable;
+combine = (
   value1: Combinable,
   value2: Combinable,
   resultFormat: ResultFormatType
-) => {
+): Combinable => {
   let result: Combinable;
   if (resultFormat === "as-num") {
     result = +value1 + +value2;
@@ -18,3 +19,17 @@ const combine = (
 console.log(combine(10, "20", "as-num"));
 console.log(combine(10, 20, "as-str"));
 console.log(combine("str", 20, "as-num"));
+
+//------------------------------------------------------ type any vs unknown
+let valueToBeAssign1: any;
+let valueToBeAssign2: unknown;
+let getValue: number;
+
+valueToBeAssign1 = 10;
+valueToBeAssign2 = 5;
+
+getValue = valueToBeAssign1; // with type any is not checked
+// getValue = valueToBeAssign2; //with tyúe unknown this is not allowed
+if (typeof valueToBeAssign2 === "number") {
+  getValue = valueToBeAssign2;
+}
